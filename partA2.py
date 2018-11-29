61a7307d82cde6074dafe5f7e71f9c673ea1c373
RONIT NATH <35529364+roy-1996@users.noreply.github.com>
2018-11-29 17:34:20 +0530

    Update partA2.py

--- partA2.py
+++ partA2.py
@@ -4,7 +4,6 @@ import io
 import sys
 import subprocess
 import time
-from sklearn import preprocessing
 from nltk.tokenize import word_tokenize,sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
